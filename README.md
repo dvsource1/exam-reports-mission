af3bee00bf2666335daa5e53f21c7db274b99fd7
dvsource <virajkaush@gmail.com>
2022-07-17 12:29:19 +0530

    setup project structure & routing

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This project was generated with [Angular CLI](https://github.com/angular/angular
 ### TODO
 
 - [x] Initial Setup
-- [ ] Project Structure & Routing
+- [x] Project Structure & Routing
 - [ ] API Integration & Services
 - [ ] Data Binding
 - [ ] Filter
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'report',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'report',
+    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./report/report-routing.module').then(
+        (m) => m.ReportRoutingModule
+      ),
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
